d28cc67f9e96444e8d39a3653364c874b924197f
eric <polvu@hotmail.fr>
2021-06-09 17:09:00 +0200

    Un peu de css et création de la page styles.jsx

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const  HomePage = (props) => {
     return ( 
 <>
-    <h1>Salut</h1>
+<div className="container mt-5">
+    <h1 className="text-center">Bienvenue sur <span className="titre_background pad">Webmusicjam</span> !</h1>
+    <h2 className="text-center mt-5">Inscrivez vous et trouvez des partenaires pour jouer quand vous voulez.</h2>
+</div>
 </>
     )
 }
